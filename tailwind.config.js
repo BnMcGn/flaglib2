38a8285db29bd5bf6860713e1bc7b466c165a872
bnmcgn@gmail.com <bnmcgn@gmail.com>
2022-09-20 08:50:38 -0700

    Tailwind working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./resources/public/cljs-out/**/*.js",
-              "./src/warflagger-web/*.lisp"],
+    content: ['./resources/public/cljs-out/**/*.js',
+              './src/warflagger-web/*.lisp',],
   theme: {
       extend: {},
       fontFamily: {
