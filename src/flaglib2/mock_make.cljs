482e81e8eeaeb782a08e72246a267de0facaf805
bnmcgn@gmail.com <bnmcgn@gmail.com>
2023-06-12 08:47:05 -0700

    Mock-make is wired up

--- src/flaglib2/mock_make.cljs
+++ src/flaglib2/mock_make.cljs
@@ -5,21 +5,40 @@
    [re-frame.core :as rf]
 
    [flaglib2.fabricate :as fab]
+   [flaglib2.stepper :as step]
 
    ))
 
 
-{:flaglib2.posters/alternate-response nil,
- :title-store {},
- :opinion-store {},
- :flaglib2.fabricate/specify-target {:on-select nil, :suppress-search-results false},
- :flaglib2.posters/opinion-failure nil,
- :server-parameters {},
- :text-store {},
- :warstats-store {},
- :flaglib2.posters/opinion-response nil,
- :flaglib2.posters/alternate-failure nil,
+(def plain-db
+  {:flaglib2.posters/alternate-response nil,
+   :title-store {},
+   :opinion-store {},
+   :flaglib2.fabricate/specify-target {:on-select nil, :suppress-search-results false},
+   :flaglib2.posters/opinion-failure nil,
+   :server-parameters {},
+   :text-store {},
+   :warstats-store {},
+   :flaglib2.posters/opinion-response nil,
+   :flaglib2.posters/alternate-failure nil,
 
- :flaglib2.fetchers/author-urls {}
+   :flaglib2.fetchers/author-urls {}})
 
- }
+(def sections
+  {:initial plain-db})
+
+(defn mock-make [{:keys [section]}]
+  [step/wf-stepper])
+
+(rf/reg-event-fx
+ :mock-make
+ (fn [{:keys [db]} _]
+   (let [section (keyword (get-in db [:server-parameters :section]))]
+     {:db (merge
+           db
+           (get sections section)
+           {:root-element mock-make})
+      :fx [
+           ;[:dispatch [:add-hooks fabricate-hooks]]
+           [:dispatch [:flaglib2.stepper/initialize fab/steps]]
+           [:mount-registered]]})))
