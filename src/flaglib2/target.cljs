3e8a0fc05b99215168f93ed1cd1ebea5a9e59b03
bnmcgn@gmail.com <bnmcgn@gmail.com>
2023-08-30 08:49:01 -0700

    Redecorated tabs

--- src/flaglib2/target.cljs
+++ src/flaglib2/target.cljs
@@ -50,11 +50,13 @@
                           (keyword tmode)
                           :article))]
     [:<> [rc/horizontal-tabs
-          :class "mt-1"
           :model current
           :tabs [{:id :article :label "Article View"}
                  {:id :comment :label "Comment View"}
                  {:id :summary :label "Summary"}]
+          :parts {:wrapper {:class "mt-2"}
+                  ;;FIXME: might need to redefine tw-tab-active func
+                  :anchor {:class "text-neutral-600"}}
           :on-change #(set! js/window.location.href
                             (uri/setParam js/window.location.href "tmode" (name %1)))]
      (case @current
