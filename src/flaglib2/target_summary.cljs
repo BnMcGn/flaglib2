91bbdf2e1c48b70fb51c505b57dcb56845c8dbec
bnmcgn@gmail.com <bnmcgn@gmail.com>
2023-09-14 06:51:56 -0700

    Tuning appearance

--- src/flaglib2/target_summary.cljs
+++ src/flaglib2/target_summary.cljs
@@ -14,8 +14,9 @@
   (str "translate(" x "," y ")"))
 
 (def bar-width 20)
-(def bar-height-max 200)
-(def centerline (+ bar-height-max 40))
+(def bar-height-max 150)
+(def m [0 0 0 0])
+(def centerline (+ bar-height-max 30))
 
 (defn positive-bar [& {:keys [style label transform height]}]
   (let [top (- centerline height)]
@@ -51,44 +52,45 @@
     label]])
 
 (defn summary-scores-chart [rooturl]
-  (let [m [50 40 50 40]
-        w (- 960 (m 1) (m 3))
-        h (- 500 (m 0) (m 2))
+  (let [w (- 110 (m 1) (m 3))
+        h (- (* 2 centerline) (m 0) (m 2))
         warstats @(rf/subscribe [:warstats-store rooturl])
-        colsize 50
-        col2 (* 2 colsize)
+        colsize 20
+        col2 70
         highest (max (:x-right warstats) (:x-wrong warstats) (:x-up warstats) (:x-down warstats))]
-    [:svg {:width (+ w (m 1) (m 3))
-           :height (+ h (m 0) (m 2))
-           :transform (translate (m 3) (m 0))}
-     [positive-bar
-      :style {:fill "#80ff80" :stroke "black" :strokeWidth "1"}
-      :label (:x-right warstats)
-      :height (misc/as-in-range
-               0 bar-height-max
-               (misc/relative-to-range 0 highest (:x-right warstats)))
-      :transform (translate colsize 0)]
-     [positive-bar
-      :style {:fill "#80ff80" :stroke "black" :strokeWidth "1"}
-      :label (:x-up warstats)
-      :height (misc/as-in-range
-               0 bar-height-max
-               (misc/relative-to-range 0 highest (:x-up warstats)))
-      :transform (translate col2 0)]
-     [negative-bar
-      :style {:fill "#ff8080" :stroke "black" :strokeWidth "1"}
-      :label (:x-wrong warstats)
-      :height (misc/as-in-range
-               0 bar-height-max
-               (misc/relative-to-range 0 highest (:x-wrong warstats)))
-      :transform (translate colsize centerline)]
-     [negative-bar
-      :style {:fill "#ff8080" :stroke "black" :strokeWidth "1"}
-      :label (:x-down warstats)
-      :height (misc/as-in-range
-               0 bar-height-max
-               (misc/relative-to-range 0 highest (:x-down warstats)))
-      :transform (translate col2 centerline)]]))
+    [:div
+     {:style {:border "1px" :background-color "antiquewhite" }}
+     [:svg {:width (+ w (m 1) (m 3))
+            :height (+ h (m 0) (m 2))
+            :transform (translate (m 3) (m 0))}
+      [positive-bar
+       :style {:fill "#80ff80" :stroke "black" :strokeWidth "1"}
+       :label (:x-up warstats)
+       :height (misc/as-in-range
+                0 bar-height-max
+                (misc/relative-to-range 0 highest (:x-up warstats)))
+       :transform (translate colsize 0)]
+      [positive-bar
+       :style {:fill "#80ff80" :stroke "black" :strokeWidth "1"}
+       :label (:x-right warstats)
+       :height (misc/as-in-range
+                0 bar-height-max
+                (misc/relative-to-range 0 highest (:x-right warstats)))
+       :transform (translate col2 0)]
+      [negative-bar
+       :style {:fill "#ff8080" :stroke "black" :strokeWidth "1"}
+       :label (:x-down warstats)
+       :height (misc/as-in-range
+                0 bar-height-max
+                (misc/relative-to-range 0 highest (:x-down warstats)))
+       :transform (translate colsize centerline)]
+      [negative-bar
+       :style {:fill "#ff8080" :stroke "black" :strokeWidth "1"}
+       :label (:x-wrong warstats)
+       :height (misc/as-in-range
+                0 bar-height-max
+                (misc/relative-to-range 0 highest (:x-wrong warstats)))
+       :transform (translate col2 centerline)]]]))
 
 (defn target-summary [& {:keys [rooturl]}]
   [:div [summary-scores-chart rooturl]])
