ce9dae315f6e4a19044a31f492e54d2c814c8845
bnmcgn@gmail.com <bnmcgn@gmail.com>
2024-10-30 07:20:04 -0700

    All tests passing

--- test/flaglib2/hixer_test.cljs
+++ test/flaglib2/hixer_test.cljs
@@ -8,7 +8,7 @@
 
 (def good1 [[:a {:href "http://there/"} "is"]
             [:div [:h1 "thing"] [:p "thing"]]
-            ['hixer/embedded-opinion]])
+            ['flaglib2.hixer/embedded-opinion]])
 
 (def bad1 [[:div {:class "less"}]])
 (def bad2 [:a :b :c])
