df85f04b68478a2eb91c50ab162dbee4e9abda46
bnmcgn@gmail.com <bnmcgn@gmail.com>
2025-01-11 09:22:24 -0800

    Added tests for in-a-word

--- test/flaglib2/mood_test.cljs
+++ test/flaglib2/mood_test.cljs
@@ -19,3 +19,25 @@
          (mood/flavor-from-own-warstats (assoc contested-warstat-1
                                                :custodial-offtopic 10)))))
 
+
+(def positive-warstat
+  {:x-up 3 :x-down 0 :x-right 0 :x-wrong 0})
+(def db-1 {:refd {"asdf" [:one]}})
+
+(deftest in-a-word
+  (is (= :contested (mood/in-a-word contested-warstat-1)))
+  (is (= :unsupported
+         (mood/in-a-word {:x-up 10 :x-down 0 :x-right 0 :x-wrong 8})))
+  (is (= :awkward
+         (mood/in-a-word {:x-up 0 :x-down 10 :x-right 10 :x-wrong 0})))
+  (is (= :ignored
+         (mood/in-a-word {:x-up 0 :x-down 0 :x-right 0 :x-wrong 0})))
+  (is (= :restricted
+         (mood/in-a-word {:x-up 10 :x-down 0 :x-right 0 :x-wrong 0 :negative-spam 8})))
+  (is (= :sidelined
+         (mood/in-a-word {:x-up 10 :x-down 0 :x-right 0 :x-wrong 0 :custodial-arcane 8})))
+  (is (= :contested
+         (mood/in-a-word {:x-up 10 :x-down 6 :x-right 0 :x-wrong 0 :custodial-arcane 6})))
+  (is (= :positive (mood/in-a-word positive-warstat :key "qwert" :db db-1)))
+  (is (= :significant (mood/in-a-word positive-warstat :key "asdf" :db db-1))))
+
