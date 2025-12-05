a360d4242aac0c1e9f9025833070ffbbfa0fd5d4
bnmcgn@gmail.com <bnmcgn@gmail.com>
2025-12-05 09:35:31 -0800

    Added excerpt warn-off test

--- src/flaglib2/mock_vis.cljs
+++ src/flaglib2/mock_vis.cljs
@@ -31,12 +31,14 @@
   {(tid 0)
    (assoc plain-opinion
           :target rooturl
+          :iid (tid 0)
           :flag :negative-dislike
           :tree-address (list (tid 0))
           :clean-comment "This is a good opinion")
    (tid 1)
    (assoc plain-opinion
           :target (tid 0)
+          :iid (tid 1)
           :flag :positive-evidence
           :tree-address (list (tid 0) (tid 1))
           :clean-comment "This is a bad opinion. It should have a warn off"
@@ -44,32 +46,52 @@
    (tid 2)
    (assoc plain-opinion
           :target rooturl
+          :iid (tid 2)
           :flag :custodial-blank
           :tree-address (list (tid 2))
           :clean-comment "This is a boring opinion. It should be hidden.")
    (tid 3)
    (assoc plain-opinion
           :target (tid 2)
+          :iid (tid 3)
           :flag :positive-like
           :tree-address (list (tid 2) (tid 3))
           :clean-comment "This is an exciting opinion.")
    (tid 4)
    (assoc plain-opinion
           :target rooturl
+          :iid (tid 4)
           :flag :negative-needs-evidence
           :tree-address (list (tid 4))
           :clean-comment "This is a sidelined opinion")
    (tid 5)
    (assoc plain-opinion
           :target (tid 4)
+          :iid (tid 5)
           :flag :positive-agree
           :tree-address (list (tid 4) (tid 5))
-          :clean-comment "This opinion is sidelined by its parent")})
+          :clean-comment "This opinion is sidelined by its parent")
+   (tid 6)
+   (assoc plain-opinion
+          :target rooturl
+          :iid (tid 6)
+          :flag :custodial-blank
+          :tree-address (list (tid 6))
+          :clean-comment "This opinion has an eeeeevil portion")
+   (tid 7)
+   (assoc plain-opinion
+          :target (tid 6)
+          :iid (tid 7)
+          :flag :negative-disturbing
+          :tree-address (list (tid 6) (tid 7))
+          :excerpt "eeeeevil"
+          :clean-comment "This targets an excerpt")})
 
 (def opinion-tree
   `((~(tid 0) (~(tid 1)))
     (~(tid 2) (~(tid 3)))
-    (~(tid 4) (~(tid 5)))))
+    (~(tid 4) (~(tid 5)))
+    (~(tid 6) (~(tid 7)))))
 
 (def basic-warstat
   {:replies-immediate 0
@@ -108,6 +130,13 @@
           :x-up 5)
    (tid 5)
    (assoc basic-warstat
+          :x-up 2)
+   (tid 6)
+   (assoc basic-warstat
+          :x-up 3
+          :negative-disturbing 4)
+   (tid 7)
+   (assoc basic-warstat
           :x-up 2)})
 
 (def vis-lister
@@ -117,7 +146,9 @@
     {:id (tid 2) :type :opinion}
     {:id (tid 3) :type :opinion}
     {:id (tid 4) :type :question}
-    {:id (tid 5) :type :opinion}]})
+    {:id (tid 5) :type :opinion}
+    {:id (tid 6) :type :opinion}
+    {:id (tid 7) :type :opinion}]})
 
 (defn mock-vis [_]
   [:div
@@ -134,6 +165,10 @@
              :opinion-tree-store
              {rooturl opinion-tree}
              :warstats-store warstats
+             :title-store
+             (into {}
+                   (for [[iid opinion] opinions]
+                     [iid {:title (:clean-comment opinion)}]))
              :thing-listers
              (assoc (:thing-listers db) :vis-lister vis-lister)
              :root-element mock-vis)
